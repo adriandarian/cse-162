335735f431c10031e9a31f5e3163e3221e459a9d
adriandarian <adarian@ucmerced.edu>
2021-01-29 15:32:54 -0800

    add: fragments example in lab 1

---
+++ Labs/Lab_1/FragmentBasics/app/src/main/java/com/example/fragmentbasics/Ipsum.java
@@ -0,0 +1,13 @@
+package com.example.fragmentbasics;
+
+public class Ipsum {
+    static String[] Headlines = {
+            "Article One",
+            "Article Two"
+    };
+
+    static String[] Articles = {
+            "Article One\n\nExcepteurpour-over occaecatsquid biodiesel umami gastropub, nullalaborumsalvia dreamcatcher fanny pack. Ullamcoculpa retro ea, trust fund excepteureiusmoddirect trade banksynisi lo-fi cray messenger bag. Nesciuntessecarlesselvage put a bird on it gluten-free, wesandersonuttrust fund twee occupy viral. Laborissmall batch scenesterpork belly, leggings utfarm-to-table aliquipyrnostrudiphoneviral next level. Craft beer dreamcatcher pinteresttruffautethnic, authentic brunch. Essesingle-origin coffee banksydo next level tempor. Velitsynth dreamcatcher, magna shoreditchin americanapparel messenger bag narwhal PBR ennui farm-to-table.",
+            "Article Two\n\nVinylwilliamsburgnon velit, master cleanse four lokobanh mi. Enimkogikeytar trust fund pop-up portlandgentrify. Non eatypewriter doloredeseruntAustin. Ad magna ethical kogimixtape next level. Aliquapork belly thundercats, utpop-up tattooed dreamcatcher kogiaccusamusphoto booth irony portland. Semiotics brunch utlocavore irure, enimetsylaborumstumptowncarlesgentrify post-ironic cray. Butcher 3 wolf moon blog synth, vegan carlesodd future."
+    };
+}
---
+++ Labs/Lab_1/FragmentBasics/app/src/main/java/com/example/fragmentbasics/NewsFragment.java
@@ -0,0 +1,100 @@
+package com.example.fragmentbasics;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NewsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NewsFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public NewsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment NewsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static NewsFragment newInstance(String param1, String param2) {
+        NewsFragment fragment = new NewsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    final static String ARG_POSITION = "position";
+    int mCurrentPosition = -1;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_news, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // During startup, check if there are arguments passed to the fragment.
+        // onStart is a good place to do this because the layout has already been
+        // applied to the fragment at this point so we can safely call this method
+        // below that sets the article text.
+
+        Bundle args = getArguments();
+        if (args != null) {
+            // Set article based on argument passed in
+            updateArticleView(args.getInt(ARG_POSITION));
+        } else if (mCurrentPosition != -1) {
+            // Set article based on saved instance state defined during onCreateView
+            updateArticleView(mCurrentPosition);
+        }
+    }
+
+    public void updateArticleView(int position) {
+        TextView article = (TextView) getActivity().findViewById(R.id.news);
+        article.setText(Ipsum.Articles[position]);
+        mCurrentPosition = position;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save the current article selection in case we need to recreate the fragment
+        outState.putInt(ARG_POSITION, mCurrentPosition);
+    }
+}
